8f90fb6abdc8bc8aeda5925104469057286c3c2e
akdilsude <akdilsude@gmail.com>
2025-04-10 11:20:09 +0300

    Add files via upload

--- build_stack.c
+++ build_stack.c
@@ -6,35 +6,53 @@
 /*   By: sakdil <sakdil@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 14:19:23 by sakdil            #+#    #+#             */
-/*   Updated: 2025/04/08 19:03:40 by sakdil           ###   ########.fr       */
+/*   Updated: 2025/04/10 11:16:29 by sakdil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static long	ft_atoi(const char *str)
+static long ft_atoi(const char *str, int *error)
 {
-	int		i;
-	long	res;
-	int		sign;
+    int     i;
+    long    res;
+    int     sign;
+    size_t  digit_count;
 
-	i = 0;
-	res = 0;
-	sign = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res = (res * 10) + (str[i] - '0');
-		i++;
-	}
-	return (res * sign);
+    i = 0;
+    res = 0;
+    sign = 1;
+    digit_count = 0;
+    *error = 0;
+
+    while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        digit_count++;
+        if (digit_count > 10 || 
+            (sign == 1 && res > (2147483647 - (str[i] - '0')) / 10) || 
+            (sign == -1 && res > (2147483648 - (str[i] - '0')) / 10))
+        {
+            *error = 1;
+            return (0);
+        }
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    res = res * sign;
+    if (str[i] != '\0')
+    {
+        *error = 1;
+        return (0);
+    }
+    return (res);
 }
 
 static void	add_node(t_list **stack, int n)
@@ -62,9 +80,9 @@ static void	add_node(t_list **stack, int n)
 	}
 }
 
-static void	continue_build_a(long n, t_list **a, char **argv, bool split_used)
+static void	continue_build_a(long n, t_list **a, char **argv, bool split_used, int error)
 {
-	if (n > 2147483647 || n < -2147483648)
+	if (error)
 	{
 		if (split_used)
 			free_errors(a, argv, split_used);
@@ -86,8 +104,10 @@ void	build_stack_a(t_list **a, char **argv, bool split_used)
 {
 	long	n;
 	int		i;
+	int		error;
 
 	i = 0;
+	error = 0;
 	while (argv[i])
 	{
 		if (is_valid_number(argv[i]))
@@ -98,9 +118,10 @@ void	build_stack_a(t_list **a, char **argv, bool split_used)
 				free_errors(a, NULL, split_used);
 			return ;
 		}
-		n = ft_atoi(argv[i]);
-		continue_build_a(n, a, argv, split_used);
-		add_node(a, (int)n);
+		n = ft_atoi(argv[i], &error);
+		continue_build_a(n, a, argv, split_used, error);
+		if (!error)
+			add_node(a, (int)n);
 		i++;
 	}
 }
